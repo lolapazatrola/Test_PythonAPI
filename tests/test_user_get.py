3bcd7c605dc25ace082a625cf4499a19335cf2ce
Danila Kazimir <Danila.Kazimir@aton.ru>
2022-10-24 16:39:37 +0300

    ex16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -31,3 +31,29 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(responce2, expected_fields)
+
+    def test_get_user_details_auth_as_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        response2 = MyRequests.get(
+            f"/user/1",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_value_by_name(
+            response2,
+            "username",
+            "Lana",
+            "Wrong username after get user details auth as another user"
+        )
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
+
+
